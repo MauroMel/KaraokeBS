42416da58e1f903ba91f3a6d3f497868102263cb
MauroMel <melmau@tiscali.it>
2026-02-23 19:07:13 +0100

    Fix styling: Tailwind build + cleanup imports

--- index.tsx
+++ index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './tailwind.css';
 import './theme.css';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
